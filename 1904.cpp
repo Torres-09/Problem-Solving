f2402e2c89c56d90cf8df3f3c2049be0b4e8a1d5
dla280 <dla612@naver.com>
2022-01-05 23:11:34 +0900

    22.01.05

---
+++ 1904.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<string>
+using namespace std;
+set<string> s[1000000];
+
+void add()
+{
+	
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+}
