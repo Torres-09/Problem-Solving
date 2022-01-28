b8b725dbf73cee32ff669a08eb3412614b4ae94e
hwan2da <76430979+Torres-09@users.noreply.github.com>
2022-01-29 00:35:05 +0900

    Add files via upload

--- 2178.cpp
+++ 2178.cpp
@@ -1,3 +1,5 @@
+// ¹Ì·Î Å½»ö
+// BFS
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -5,62 +7,35 @@ using namespace std;
 int graph[101][101];
 bool visit[101][101];
 int n, m;
-queue<pair<int,int>> q;
+queue<pair<int, int>> q;
+int dx[4] = { -1 , 1, 0, 0 };
+int dy[4] = { 0, 0 , -1, 1 };
 
 void BFS(int u, int v)
 {
 	pair<int, int> point;
 	q.push({ u,v });
+	visit[u][v] = true;
 	while (!q.empty())
 	{
 		point = q.front();
 		q.pop();
 
-		if(point.first != 0)
-			if (visit[point.first - 1][point.second] == false)
+		for (int i = 0; i < 4; i++)
+		{
+			int next_x = point.first + dx[i];
+			int next_y = point.second + dy[i];
+
+			if (0 <= next_x && next_x <= n && 0 <= next_y && next_y <= m)
 			{
-				if (graph[point.first - 1][point.second] != 0)
+				if (visit[next_x][next_y] == false && graph[next_x][next_y] != 0)
 				{
-					q.push({ point.first - 1, point.second });
-					visit[point.first - 1][point.second] = true;
-					graph[point.first - 1][point.second] += graph[point.first][point.second];
+					q.push({ next_x,next_y });
+					visit[next_x][next_y] = true;
+					graph[next_x][next_y] += graph[point.first][point.second];
 				}
 			}
-
-		if(point.first!=n-1)
-			if (visit[point.first + 1][point.second] == false)
-			{
-				if (graph[point.first + 1][point.second] != 0)
-				{
-					q.push({ point.first + 1,point.second });
-					visit[point.first + 1][point.second] = true;
-					graph[point.first + 1][point.second] += graph[point.first][point.second];
-				}
-			}
-
-		if(point.second !=0)
-			if (visit[point.first][point.second - 1] == false)
-			{
-				if (graph[point.first][point.second - 1] != 0)
-				{
-					q.push({ point.first,point.second - 1 });
-					visit[point.first][point.second - 1] = true;
-					graph[point.first ][point.second-1] += graph[point.first][point.second];
-				}
-			}
-
-		if(point.second != m)
-			if (visit[point.first][point.second + 1] == false)
-			{
-				if (graph[point.first][point.second + 1] != 0)
-				{
-					q.push({ point.first,point.second + 1 });
-					visit[point.first][point.second + 1] = true;
-					graph[point.first][point.second+1] += graph[point.first][point.second];
-				}
-			}
-
-
+		}
 	}
 }
 
@@ -86,5 +61,5 @@ int main()
 	}
 
 	BFS(0, 0);
-	cout << graph[n-1][m-1];
+	cout << graph[n - 1][m - 1];
 }
--- 7576.cpp
+++ 7576.cpp
@@ -2,12 +2,15 @@
 #include<iostream>
 #include<algorithm>
 #include<queue>
+#include<string.h>
 using namespace std;
 int m, n;
 int graph[1001][1001];
 queue<pair<int, int>> q;
 int max_value = 1;
 pair<int, int> point;
+int dx[4] = { -1 , 1, 0, 0 };
+int dy[4] = { 0, 0 , -1, 1 };
 
 void BFS()
 {
@@ -16,40 +19,29 @@ void BFS()
 		point = q.front();
 		q.pop();
 
-		if (point.first != 0)
-			if (graph[point.first - 1][point.second] == 0)
-			{
-				q.push({ point.first - 1, point.second });
-				graph[point.first - 1][point.second] += graph[point.first][point.second] + 1;
-			}
+		for (int i = 0; i < 4; i++)
+		{
+			int next_x = point.first + dx[i];
+			int next_y = point.second + dy[i];
 
-		if (point.first != n - 1)
-			if (graph[point.first + 1][point.second] == 0)
+			if (0 <= next_x && next_x <= n && 0 <= next_y && next_y <= m)
 			{
-				q.push({ point.first + 1,point.second });
-				graph[point.first + 1][point.second] += graph[point.first][point.second] + 1;
+				if (graph[next_x][next_y] == 0)
+				{
+					q.push({ next_x,next_y });
+					graph[next_x][next_y] += graph[point.first][point.second] + 1;
+				}
 			}
-
-		if (point.second != 0)
-
-			if (graph[point.first][point.second - 1] == 0)
-			{
-				q.push({ point.first,point.second - 1 });
-				graph[point.first][point.second - 1] += graph[point.first][point.second] + 1;
-			}
-
-		if (point.second != m)
-			if (graph[point.first][point.second + 1] == 0)
-			{
-				q.push({ point.first,point.second + 1 });
-				graph[point.first][point.second + 1] += graph[point.first][point.second] + 1;
-			}
+		}
 	}
 }
 
 int main()
 {
-
+	for (int i = 0; i < 1001; i++)
+	{
+		memset(graph[i], -2, sizeof(int) * 1001);
+	}
 	cin >> m >> n;
 	int k;
 	int zero_count = 0;
