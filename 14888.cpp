7b96469da0b8a615650e7d131b59fde914554ace
hwan2da <76430979+Torres-09@users.noreply.github.com>
2022-02-08 23:36:37 +0900

    연산자 끼워넣기 - 삼성 소프트웨어 역량 테스트 기출

--- 14888.cpp
+++ 14888.cpp
@@ -1,33 +1,53 @@
+// 연산자 끼워넣기
 #include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
-vector<int> number;
-vector<int> oper;
-// + - * /
+int v[12];
+int n;
+int a, b, c, d;
+vector<int> value;
 
+void solve(int count, int q, int w, int e, int r, int result)
+{
+	if (q > a || w > b || e > c || r > d)
+	{
+		return;
+	}
+
+	if (count == n - 1)
+	{
+		value.push_back(result);
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (i == 0)
+			solve(count + 1, q + 1, w, e, r, result + v[count + 1]);
+		else if (i == 1)
+			solve(count + 1, q, w + 1, e, r, result - v[count + 1]);
+		else if (i == 2)
+			solve(count + 1, q, w, e + 1, r, result * v[count + 1]);
+		else if (i == 3)
+			solve(count + 1, q, w, e, r + 1, result / v[count + 1]);
+	}
+}
 
 int main()
 {
-	int N;
-	cin >> N;
+	cin >> n;
 
-	int oper_num = N - 1;
-
-	while (N--)
+	for (int i = 0; i < n; i++)
 	{
-		int num;
-		cin >> num;
-		number.push_back(num);
+		cin >> v[i];
 	}
 
-	reverse(begin(number), end(number));
-	
-	while (oper_num--)
-	{
-		int num;	
-		cin >> num;
-		oper.push_back(num);
-	}
-	
+	cin >> a >> b >> c >> d;
+
+	solve(0, 0, 0, 0, 0, v[0]);
+	sort(value.begin(), value.end());
+
+
+	cout << value.back() << "\n" << value.front();
 }
