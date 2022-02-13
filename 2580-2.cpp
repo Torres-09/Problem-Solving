caebeee8cb5bbf8b4a37e21eebd3aa30f54589b1
hwan2da <76430979+Torres-09@users.noreply.github.com>
2022-02-13 22:39:56 +0900

    스도쿠
    ㅜㅜ

--- 2580-2.cpp
+++ 2580-2.cpp
@@ -1,44 +1,56 @@
 // 스도쿠
 #include<iostream>
 #include<queue>
+#include<stack>
+#include<vector>
+#include<list>
 using namespace std;
-int map[10][10];
+int map[9][9];
 bool check_row[10][10];
 bool check_col[10][10];
 bool check_box[10][10];
-queue<pair<int, int>> q; // 해결해야 할 좌표
+list<pair<int, int>> l;
+stack<pair<int, int>> s;	// 해결해야 하는 좌표
 
 void solve()
 {
-	while (!q.empty())
+	if (l.empty())
 	{
-		bool result = false;
-		pair<int, int> point = q.front();
-
-		for (int i = 1; i <= 9; i++)
+		for (int i = 0; i < 9; i++)
 		{
-			if (check_row[point.first][i] == false && check_col[point.second][i] == false &&
-				check_box[(point.first / 3) * 3 + point.second / 3][i] == false)
+			for (int j = 0; j < 9; j++)
 			{
-				map[point.first][point.second] = i;
-				check_row[point.first][i] = true;
-				check_col[point.second][i] = true;
-				check_box[(point.first / 3) * 3 + point.second / 3][i] = true;
-				result = true;
-				break;
+				cout << map[i][j] << " ";
 			}
+			cout << "\n";
 		}
 
-		if (result == true)
+		exit(0);
+	}
+
+	pair<int, int> point = l.front();
+	l.pop_front();
+
+	for (int i = 1; i <= 9; i++)
+	{
+		if (check_row[point.first][i] == false && check_col[point.second][i] == false &&
+			check_box[(point.first / 3) * 3 + point.second / 3][i] == false)
 		{
-			q.pop();
-		}
-		else
-		{
-			q.pop();
-			q.push(point);
+			map[point.first][point.second] = i;
+			check_row[point.first][i] = true;
+			check_col[point.second][i] = true;
+			check_box[(point.first / 3) * 3 + point.second / 3][i] = true;
+
+			solve();
+
+			map[point.first][point.second] = 0;
+			check_row[point.first][i] = false;
+			check_col[point.second][i] = false;
+			check_box[(point.first / 3) * 3 + point.second / 3][i] = false;
 		}
 	}
+
+	l.push_front({ point.first,point.second });
 }
 
 int main()
@@ -54,25 +66,16 @@ int main()
 			cin >> map[i][j];
 
 			if (map[i][j] == 0)
-			{
-				q.push({ i,j });
-			}
+				l.push_back({ i,j });
 			else
 			{
 				check_row[i][map[i][j]] = true;	// 가로
 				check_col[j][map[i][j]] = true;	// 세로
-				check_box[(i / 3) * 3 + j / 3][map[i][j]] = true;	//박스
+				check_box[(i / 3) * 3 + j / 3][map[i][j]] = true;
 			}
 		}
 	}
 
 	solve();
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			cout << map[i][j] << " ";
-		}
-		cout << "\n";
-	}
+
 }
