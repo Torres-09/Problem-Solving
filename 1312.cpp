566942c7f3c1bc2a279dd7bbc7ed945d4e233954
hwan2da <76430979+Torres-09@users.noreply.github.com>
2022-06-03 23:17:05 +0900

    소수

--- 1312.cpp
+++ 1312.cpp
@@ -3,46 +3,18 @@ using namespace std;
 
 int main()
 {
-	long long int a, b, c;
+	int a, b, c;
 	cin >> a >> b >> c;
-
 	a = a % b;
 
-	vector<int> v;
-	while (1)
+	int ans;
+	
+	while (c--)
 	{
-		if (a / b < 1)
-		{
-			a *= 10;
-		}
-		else
-		{
-			if (!v.empty())
-			{
-				if (v.front() == a / b)
-					break;
-			}
-
-			v.push_back(a / b);
-			a %= b;
-			a *= 10;
-
-			if (a == 0)
-				break;
-		}
+		a *= 10;
+		ans = a / b;
+		a %= b;
 	}
 
-	if (v.size() == 0)
-	{
-		cout << "0";
-	}
-	else if (v.size() == 1)
-	{
-		cout << v[0];
-	}
-	else
-	{
-		int ans = c % v.size();
-		cout << v[ans - 1];
-	}
+	cout << ans;
 }
