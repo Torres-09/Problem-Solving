a8b280705033ef9a8e8a20c0464ecd5db89211e7
hwan2da <76430979+Torres-09@users.noreply.github.com>
2022-06-24 22:27:53 +0900

    별 찍기 - 3

---
+++ 2440.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = n; i >0; i--)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			cout << '*';
+		}
+		cout << "\n";
+	}
+}
