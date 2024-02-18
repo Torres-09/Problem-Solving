ddc7c7d2bc44e22381c73dc81488f8586a06d0c4
hwan2da <dla612@naver.com>
2024-02-18 17:30:12 +0900

    재귀의 귀재

---
+++ baekjoon/java/p25501.java
@@ -0,0 +1,37 @@
+package com.baekjoon.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p25501 {
+	static int N;
+	static int count;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			count = 0;
+			st = new StringTokenizer(br.readLine());
+			String input = st.nextToken();
+			System.out.println(isPalindrome(input) + " ".concat(String.valueOf(count)));
+		}
+	}
+
+	public static int recursion(String s, int l, int r) {
+		count++;
+		if (l >= r)
+			return 1;
+		else if (s.charAt(l) != s.charAt(r))
+			return 0;
+		else
+			return recursion(s, l + 1, r - 1);
+	}
+
+	public static int isPalindrome(String s) {
+		return recursion(s, 0, s.length() - 1);
+	}
+}
