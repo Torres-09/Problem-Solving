44fdf8010201d43edbf6cef25b462c9b51e5dba5
dla612 <dla612@naver.com>
2024-02-29 16:17:43 +0900

    뒤집기

---
+++ baekjoon/java/p1439.java
@@ -0,0 +1,40 @@
+package com.baekjoon.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1439 {
+    static String arr;
+    static int countZero;
+    static int countOne;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        arr = br.readLine();
+
+        for (int i = 0; i < arr.length(); i++) {
+            if (arr.charAt(i) == '1') {
+                int start = i;
+                countZero++;
+                for (int j = i; j < arr.length(); j++) {
+                    if (arr.charAt(start) != arr.charAt(j)) break;
+
+                    i++;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length(); i++) {
+            if (arr.charAt(i) == '0') {
+                int start = i;
+                countOne++;
+                for (int j = i; j < arr.length(); j++) {
+                    if (arr.charAt(start) != arr.charAt(j)) break;
+
+                    i++;
+                }
+            }
+        }
+
+        System.out.println(Math.min(countZero, countOne));
+    }
+}
