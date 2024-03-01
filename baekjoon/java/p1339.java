031449c1bcce37db9ae0b0bada4104c76f493329
hwan2da <dla612@naver.com>
2024-03-01 19:27:35 +0900

    단어 수학

--- baekjoon/java/p1339.java
+++ baekjoon/java/p1339.java
@@ -4,59 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 
 public class p1339 {
-    static int N;
-    static ArrayList<String> list;
-    static HashMap<Character, Integer> map;
-    static int num;
-    static int[] answer;
-    static ArrayList<int[]> place;
+	static int N;
+	static ArrayList<String> list;
+	static HashMap<Character, Integer> map;
+	static ArrayList<Integer> answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        list = new ArrayList<>();
-        map = new HashMap<>();
-        num = 9;
-        answer = new int[N];
-        place = new ArrayList<>();
-        // { A, 1 }
-        // {
-        int index = 0;
-        for (int i = 0; i < N; i++) {
-            list.add(br.readLine());
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		list = new ArrayList<>();
+		map = new HashMap<>();
+		answer = new ArrayList<>();
 
-        for (int i = 0; i < list.size(); i++) {
-            String s = list.get(i);
-            for (int j = 0; j < s.length(); j++) {
-                place.add(new int[]{s.charAt(j), s.length() - j});
-            }
-        }
+		for (int i = 0; i < N; i++) {
+			list.add(br.readLine());
+		}
 
-        place.sort((o1, o2) -> Integer.compare(o2[1], o1[1]));
+		for (String s : list) {
+			for (int i = 0; i < s.length(); i++) {
+				map.put(s.charAt(i), (int)(map.getOrDefault(s.charAt(i), 0) + Math.pow(10, s.length() - i - 1)));
+			}
+		}
 
-        for (int i = 0; i < place.size(); i++) {
-            int alphaNum = place.get(i)[0];
-            if(!map.containsKey((char)alphaNum)) map.put((char) alphaNum, num--);
-        }
-
-        for (String s : list) {
-            int plus = 0;
-            for (int i = 0; i < s.length(); i++) {
-                plus *= 10;
-                plus += map.get(s.charAt(i));
-            }
-            answer[index++] = plus;
-        }
-        int sum = 0;
-        for (int i : answer) {
-            sum += i;
-        }
-        System.out.println(sum);
-    }
+		answer.addAll(map.values());
+		answer.sort((o1, o2) -> Integer.compare(o2, o1));
+		int sum = 0;
+		int mul = 9;
+		for (int i : answer) {
+			sum += i * mul;
+			mul--;
+		}
+		System.out.println(sum);
+	}
 }
